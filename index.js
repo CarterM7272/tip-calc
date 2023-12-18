d23ab1c368066762cd4294743f256a7823ff9a30
cmarkham7272@gmail.com <cmarkham7272@gmail.com>
2023-12-18 14:32:49 -0600

    created functionality of calculator but need to fix error message color and display amount

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-const calcBtn = document.getElementById("calcBtn")
+const calcBtn = document.getElementById("calcBtn");
+const tipPercentInput = document.getElementById("tipPercent");
+const billAmountInput = document.getElementById("billAmount");
+const tipTotal = document.getElementById("tipTotal");
+
+const calculateTip = () => {
+
+  const billAmount = parseFloat(billAmountInput.value);
+  const tipPercent = parseFloat(tipPercentInput.value);
+
+
+  if (isNaN(billAmount) || isNaN(tipPercent) || billAmount === 0 || tipPercent === 0) {
+    let error = document.createElement("h2");
+    error.innerHTML = 'Please enter a valid bill amount and tip percent!';
+    
+    document.body.appendChild(error)
+    error.classList.add(error)
+  }
+
+
+  const newTotal = billAmount * (1 + tipPercent / 100);
+
+
+  tipTotal.innerHTML = newTotal.toFixed(2);
+};
+
+
+calcBtn.addEventListener("click", calculateTip);
